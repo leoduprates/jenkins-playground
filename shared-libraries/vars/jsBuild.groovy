85fe4860e32c5e4559a55b031eae8613d748385f
Leonardo Duprates <lduprates@gmail.com>
2023-03-01 15:45:18 +0000

    Update jsBuild.groovy

--- shared-libraries/vars/jsBuild.groovy
+++ shared-libraries/vars/jsBuild.groovy
@@ -12,7 +12,7 @@ def call(Map config = [:]) {
       stages {
           stage('Checkout') {
               steps {
-                  git(url: config.repo, branch: BRANCH)
+                  git(url: config.repository, branch: BRANCH)
               }
           }
           stage('Install Dependencies') {
