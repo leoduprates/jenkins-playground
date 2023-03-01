ab657c3a9ef0034eca520b77d1f9f3516924a503
Leonardo Duprates <lduprates@gmail.com>
2023-03-01 15:26:51 +0000

    Create jsBuild.groovy

---
+++ shared-libraries/vars/jsBuild.groovy
@@ -0,0 +1,40 @@
+def call(Map config = [:]) {
+  pipeline {
+      environment {
+          PUPPETEER_SKIP_DOWNLOAD = 'true'
+      }
+      agent {
+          docker { image config.image }
+      }
+      parameters {
+          string(name: 'BRANCH', defaultValue: 'main', description: 'Branch to build')
+      }
+      stages {
+          stage('Checkout') {
+              steps {
+                  git(url: config.repo, branch: context.BRANCH)
+              }
+          }
+          stage('Install Dependencies') {
+              steps {
+                  sh('npm install')
+              }
+          }
+          stage('Run Unit Tests') {
+              steps {
+                  sh('npm run test:unit')
+              }
+          }
+          stage('Run Integration Tests') {
+              steps {
+                  sh('npm run test:integration')
+              }
+          }
+          stage('Run Test Coverage') {
+              steps {
+                  sh('npm run coverage -- --testPathIgnorePatterns "./tests/e2e/"')
+              }
+          }
+      }
+  }
+}
