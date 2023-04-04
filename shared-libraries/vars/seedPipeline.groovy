7452066a51107e8fbf4243ec8d86db66a6efa316
Leonardo Duprates <lduprates@gmail.com>
2023-04-04 17:30:48 +0100

    Update seedPipeline.groovy

--- shared-libraries/vars/seedPipeline.groovy
+++ shared-libraries/vars/seedPipeline.groovy
@@ -2,21 +2,27 @@ import org.jenkinsci.plugins.workflow.job.WorkflowJob
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
 
 def call(Map config = [:]) {
-    def pipelineScript = """
-      pipeline {
-        agent any
-        stages {
-          stage('Example') {
-            steps {
-              echo 'Hello, World!'
-            }
-          }
-        }
-      }
-      """
+    git(branch: 'main', url: 'https://github.com/leoduprates/jenkins-playground.git')
+    
+    def yaml = readYaml(file: "./pipelines.yaml")
+    
+    yaml.pipeline.each { pipeline ->
+         def pipelineScript = """
+              pipeline {
+                agent ${pipeline.agent}
+                stages {
+                  stage('Example') {
+                    steps {
+                      echo 'Hello, World!'
+                    }
+                  }
+                }
+              }
+              """
 
-    def myPipeline = createOrUpdatePipeline('my-pipeline', pipelineScript)
-    myPipeline.save()
+        def myPipeline = createOrUpdatePipeline(pipeline.name, pipelineScript)
+        myPipeline.save()
+    }
 }
 
 def createOrUpdatePipeline(String name, String script) {
